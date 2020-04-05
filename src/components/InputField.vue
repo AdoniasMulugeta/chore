05d36332d6c878e00b48081eda13c91d4f4d8c02
Adonias Mulugeta <mulugeta.adonias@gmail.com>
2020-04-05 14:13:42 +0300

    Made the input field to a separate component

---
+++ src/components/InputField.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <label for="todo-field"></label>
+        <input id="todo-field" :placeholder="placeholder" v-model="newInput" @keyup.enter="input">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputField",
+        props: {
+            placeholder: { type: String, default: 'Input Here ...' },
+        },
+        data: () => ({
+            newInput: '',
+        }),
+        methods: {
+            input() {
+                this.$emit('input', this.newInput)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
