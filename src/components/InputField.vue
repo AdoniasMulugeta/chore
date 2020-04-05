9da470d4157c4cde3f0a09b395b9478ba18926ed
Adonias Mulugeta <mulugeta.adonias@gmail.com>
2020-04-05 16:28:21 +0300

    Added custom auto focus directive on input field.

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <label for="todo-field"></label>
-        <input id="todo-field" :placeholder="placeholder" v-model="newInput" @keyup.enter="input">
+        <input v-focus id="todo-field" :placeholder="placeholder" v-model="newInput" @keyup.enter="input">
     </div>
 </template>
 
@@ -16,12 +16,24 @@
         }),
         methods: {
             input() {
-                this.$emit('input', this.newInput)
+                this.$emit('input', this.newInput);
+                this.newInput = '';
+            }
+        },
+        directives: {
+            focus: {
+                // directive definition
+                inserted: function (el) {
+                    el.focus()
+                }
             }
         }
+
     }
 </script>
 
 <style scoped>
-
+    #todo-field{
+        margin-bottom: 10px;
+    }
 </style>
