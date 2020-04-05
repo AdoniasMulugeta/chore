4c1c79f7080eb78bdd901a163d8b0a8e87343258
Adonias Mulugeta <mulugeta.adonias@gmail.com>
2020-04-05 16:29:08 +0300

    Added a TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="todo-item" :key="todo.key" v-bind="todo">
+        <input id='todo-complete' type="checkbox" v-model="todo.completed">
+        <input v-if="todo.editing" class="todo-name" type="text" @keyup.enter="doneEditTodo" @blur="doneEditTodo" v-model="todo.name">
+        <div ref="todo_Edit" v-else id="todo.id" class="todo-name"  @dblclick="editTodo">{{ todo.name }}</div>
+        <div class="remove-todo" @click="removeTodo">&times;</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props : { todo: { name: String, id: Number, completed: Boolean, editing: Boolean } },
+        methods: {
+            doneEditTodo(){
+                this.todo.editing = false;
+            },
+            editTodo(){
+                this.todo.editing = true;
+                this.$refs.todo_edit.focus();
+            },
+            removeTodo() {
+                this.$emit('removeTodo', this.todo)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .todo-item{
+        margin-bottom: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        animation-duration: 0.3s;
+    }
+    .todo-name{
+        font-size: 18px;
+        font-family: Monaco,serif;
+        align-self: flex-start;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        text-align: center;
+    }
+    .remove-todo {
+        cursor: pointer;
+        margin-left: 14px;
+    }
+</style>
